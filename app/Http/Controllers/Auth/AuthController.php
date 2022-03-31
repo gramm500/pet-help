0d968ab690add5506b678b0fe5a9ab9aae82dd72
nikul <nikul.victor@gmail.com>
2022-03-31 09:16:59 +0300

    add first and last name fields to users table

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -6,12 +6,13 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Requests\SignUpRequest;
 use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    public function signUp(SignUpRequest $request)
+    public function signUp(SignUpRequest $request): JsonResponse
     {
         $requestData = $request->validated();
         $requestData['password'] = Hash::make($requestData['password']);
